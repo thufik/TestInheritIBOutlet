7c142067d9a439eed207525e587bbe92b3db4cd6
Fellipe Thufik <fellipe.hoashi@itau-unibanco.com.br>
2019-01-28 14:17:32 -0200

    First Commit

---
+++ TestInheritWithViewController/MasterViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  TestInheritWithViewController
+//
+//  Created by EquipeSuporteAplicacao on 6/28/18.
+//  Copyright © 2018 Thufik. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UIViewController {
+
+    
+    @IBOutlet weak var lblTeste: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
